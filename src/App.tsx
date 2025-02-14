a4ff61f7570d4dae59f8678627215af4d8daa26f
eh-munna <emran.h.munna@gmail.com>
2025-02-14 18:37:37 +0100

    redux basic setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import ReduxPlayground from './components/ReduxPlayground/ReduxPlayground';
+
+function App() {
+  return (
+    <>
+      <ReduxPlayground />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ReduxPlayground/ReduxPlayground.tsx
@@ -0,0 +1,15 @@
+import Counter from './Counter/Counter';
+
+export default function ReduxPlayground() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold my-4 text-center">
+        Playing with Redux Toys
+      </h1>
+
+      <div className="flex flex-col gap-6">
+        <Counter />
+      </div>
+    </>
+  );
+}
